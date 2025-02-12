6a9bcc66f3e6f3878754668e1c88b97f8b57acc6
Jeff Mitchell <jeffreytm@gmail.com>
2025-02-12 10:51:27 -0500

    Created backend folder. Added spring boot to folder

---
+++ backend/pet-find/src/main/java/com/mycapstone/pet_find/PetFindApplication.java
@@ -0,0 +1,13 @@
+package com.mycapstone.pet_find;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetFindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetFindApplication.class, args);
+	}
+
+}
---
+++ backend/pet-find/src/test/java/com/mycapstone/pet_find/PetFindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycapstone.pet_find;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetFindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
